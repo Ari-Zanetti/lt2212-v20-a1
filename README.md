88b117dac29f9162d832e44a8a3463c75da697ff
Arianna Zanetti <guszanear@GU.GU.SE@mltgpu.flov.gu.se>
2020-02-19 11:45:45 +0100

    Assignment 1 - upload

--- README.md
+++ README.md
@@ -1,4 +1,34 @@
 # LT2212 V20 Assignment 1
 
-Put any documentation here including any answers to the questions in the 
-assignment on Canvas.
+Part 1 - Convert the data into a dataframe
+
+Every document is converted into a list of string using the function split().
+The words are lowercased so that there is no difference if they appear at the beginning or in the middle of a sentence, even if this could be a problem in presence of acronyms or proper names.
+Before creating the DataFrame, the words that appear in the string.punctuation list are removed.
+
+Part 2 - visualize
+
+To obtain the desired visualization, the DataFrame is pivoted on the class column, specifying "sum" as the aggregation function. 
+This way, for every word (the column containing the name of the document is not considered because it is not numeric) we will have two values:
+	- the count of occurrences of the words in the crude class
+	- the count of occurrences of the words in the grain class
+
+Part 3 - tf-idf
+
+To calculate tf-idf we need 3 different values:
+	- the total number of documents, that we can obtain looking at the number of rows in the DataFrame
+	- the frequency of a word in a document, that we already have being the value of every "word column" (not class name or file name) in the DataFrame
+	- the number of documents that contain the word, that we can obtain by counting the number of rows with a value greater than 0.
+
+Part 4 - visualize again
+
+We notice that, compared to the chart in part2, there are less function words, like articles ("the", "a"), or prepositions ("of", "in"), and more content words, specific of different types of documents, like "oil" for the class "crude", that was the tenth word with the raw count, and "wheat" for the class "grain".
+
+Part Bonus
+
+The function part_bonus(df) takes a DataFrame as input, separates the class column from the others creating two DataFrames: "data" and "labels".
+Then, it uses the function "train_test_split" contained in the sklearn module to shuffle the DataFrame and split it in two parts to use as train test and test set, and it creates a SVM Classifier using the training data as model.
+It returns a value corresponding to the accuracy calculated over the test set with the function "accuracy_score" of the sklearn module.
+Running the classification several times, the accuracy ranges between 94 and 97 % for the raw counts and between 95 and 98 % for the tfidf counts.
+This is because, with tfidf, the words that are more specific for a document have a higher weight, so the classifier can create a better model of the training set.
+
--- a1.py
+++ a1.py
@@ -3,27 +3,76 @@ import sys
 import pandas as pd
 import numpy as np
 import numpy.random as npr
-# ADD ANY OTHER IMPORTS YOU LIKE
+from glob import glob
+from collections import Counter
+import matplotlib.pyplot as plt
+import math
+import string
+from sklearn.svm import SVC
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 
-# DO NOT CHANGE THE SIGNATURES OF ANY DEFINED FUNCTIONS.
-# YOU CAN ADD "HELPER" FUNCTIONS IF YOU LIKE.
 
-def part1_load(folder1, folder2):
-    # CHANGE WHATEVER YOU WANT *INSIDE* THIS FUNCTION.
-    return pd.DataFrame(npr.randn(2,2)) # DUMMY RETURN
+CLASS_COLUMN = "classname"
+FILE_COLUMN = "filename"
 
-def part2_vis(df):
-    # DO NOT CHANGE
-    assert isinstance(df, pd.DataFrame)
+# Utility function to build a class model in the form of a list of dictionaries.
+# Each dictionary represents a row of the DataFrame {classname, filename, word1, word2, ...}
+# Only the words that appear in the current document are added to the dictionary,
+# the pandas DataFrame will automatically add the columns and fill the missing values with NaNs.
+def __build_class_model(files, class_name, corpus_list):
+	for filename in files:
+		document_dict = {CLASS_COLUMN : class_name, FILE_COLUMN : filename}
+		filestring = ""
+		with open(filename, "r") as thefile:
+			for line in thefile:
+				filestring += "\n" + line
+		text = filestring.lower().split() # lowercase so the words are not treated differently if they appear at the beginning of a sentence.
+		text = [word for word in text if word not in string.punctuation] # removes punctuation marks
+		document_dict.update(Counter(text))
+		corpus_list.append(document_dict)
+	return corpus_list
 
-    # CHANGE WHAT YOU WANT HERE
-    return df.plot(kind="bar")
+# folder1 and folder2 contain the path to the folders we want to use as corpus.
+# The name of the folder is used as name for the relative class.
+# n is an optional parameter we can use to change the minimum number of occurrences to consider.
+def part1_load(folder1, folder2, n=100):
+	allfiles1 = glob("{}/*.txt".format(folder1))
+	allfiles2 = glob("{}/*.txt".format(folder2))
+	corpus_list = __build_class_model(allfiles1, folder1, [])
+	corpus_list = __build_class_model(allfiles2, folder2, corpus_list)
+	corpus_df = pd.DataFrame(corpus_list)
+	corpus_df = corpus_df.fillna(0)
+	corpus_df = corpus_df.set_index([CLASS_COLUMN, FILE_COLUMN]) # Changes the shape of the DataFrame to consider only the columns relative to words
+	corpus_df = corpus_df.loc[:, corpus_df.sum(axis=0) > n]
+	return corpus_df.reset_index()
+
+def part2_vis(df, m=10):
+	assert isinstance(df, pd.DataFrame)
+
+	table = pd.pivot_table(df, index=[CLASS_COLUMN], aggfunc=np.sum) # Pivoting the table on the class_column, we obtain the total frequencies of a word for that class in the corpus
+	m_columns = table.sum(axis=0).sort_values(ascending=False)[:m]
+	plot_table = table.loc[:, m_columns.index].T # Transpose the DataFrame to obtain the desired visualization
+
+	return plot_table.plot(kind="bar")
 
 def part3_tfidf(df):
-    # DO NOT CHANGE
-    assert isinstance(df, pd.DataFrame)
+	assert isinstance(df, pd.DataFrame)
 
-    # CHANGE WHAT YOU WANT HERE
-    return df #DUMMY RETURN
+	total_docs = len(df.index)
+	words_only = df.select_dtypes(include='number') # Copies in a new DataFrame only the numerical columns (frequencies of the words)
+	words_only = words_only.apply(lambda x: x * math.log(total_docs / (x > 0).sum()), axis=0) # Applies the tfidf to every value in the DataFrame
+	df.update(words_only) # Applies the changes to the original DataFrame
+	return df
 
-# ADD WHATEVER YOU NEED HERE, INCLUDING BONUS CODE.
+# df is the DataFrame used to train and test the classifier
+# The function uses a SVM classifier to create a model and use it on the test data to calculate accuracy
+def part_bonus(df):
+	svclassifier = SVC(kernel='linear')
+	df = df.set_index(FILE_COLUMN)
+	data = df.drop(CLASS_COLUMN, axis=1)
+	labels = df[CLASS_COLUMN]
+	training_data, test_data, training_labels, test_labels = train_test_split(data, labels, test_size = 0.20) # Shuffles and splits the data in test and training set
+	svclassifier.fit(training_data, training_labels)
+	predicted_test_labels = svclassifier.predict(test_data)
+	return accuracy_score(test_labels, predicted_test_labels)
